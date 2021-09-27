83ca19dc210797dc8beda6d954e3be7893af578f
NIk <ivanovnikita000@gmail.com>
2021-09-27 23:48:15 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Nikita Ivanov");
+        System.out.println("11.09.2022");
+    }
+}
+
