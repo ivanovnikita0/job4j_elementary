81165d3a9f8a2a4febeaff50994f4a706d3a421f
ivanovnikita0 <ivanovnikita000@gmail.com>
2021-11-28 01:22:26 +0300

    0. Операции сравнения [#235631 #233552]

--- src/main/java/ru/job4j/condition/Liken.java
+++ src/main/java/ru/job4j/condition/Liken.java
@@ -3,7 +3,7 @@ package ru.job4j.condition;
 public class Liken {
     public static void main(String[] args) {
         int first = 10;
-        int second = 6;
+        int second = 7;
 
         boolean result = first > second;
         System.out.println(result);
