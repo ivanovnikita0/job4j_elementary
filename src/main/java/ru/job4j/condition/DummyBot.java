cefcb94b5093cd4acabcb328ba31ec1ea837b140
ivanovnikita0 <ivanovnikita000@gmail.com>
2021-11-28 03:13:51 +0300

    4.1.2. Глупый бот [#31323]

---
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,23 @@
+package ru.job4j.condition;
+
+public class DummyBot {
+
+    public static String answer(String question) {
+        String rs1 = "Это ставит меня в тупик. Задайте другой вопрос.";
+
+        if ("Привет, Бот.".equals(question)) {
+          rs1 = "Привет, умник.";
+        } else if ("Пока.".equals(question)) {
+            rs1 = "До скорой встречи.";
+        }
+        return rs1;
+    }
+
+    public static void main(String[] args) {
+        String rsl = DummyBot.answer("Привет, Бот.");
+        System.out.println(rsl);
+        rsl = DummyBot.answer("Пока.");
+        System.out.println(rsl);
+
+    }
+}
